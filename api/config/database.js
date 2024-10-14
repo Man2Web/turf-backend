41ce5d6f1b196a7cccd997e0d5252ef841def6ca
Harsha-Vardhan-24 <javvajiharshavardhan.24@gmail.com>
2024-10-14 14:39:09 +0530

    updated the files and folders

--- api/config/database.js
+++ api/config/database.js
@@ -1,14 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_CONNECTION,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.DATABASE_LOCAL,
+  ssl: false,
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
 };
-
